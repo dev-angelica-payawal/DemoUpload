8c84cc8f73bd309675168d7e80a5bb606a58c85b
dev-angelica-payawal <dev.angelica.payawal@gmail.com>
2018-02-25 08:27:02 +0800

    Initial commit

---
+++ DemoUpload/Controllers/BrandController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoUpload.Models;
+
+namespace DemoUpload.Controllers
+{
+    public class BrandController : Controller
+    {
+       
+            private ApplicationDbContext _context;
+
+            public BrandController()
+            {
+                _context = new ApplicationDbContext();
+            }
+
+            protected override void Dispose(bool disposing)
+            {
+                _context.Dispose();
+            }
+
+            // GET: Product
+            public ViewResult Index()
+            {
+              //  var products = _context.Brand.ToList();
+
+
+
+                return View();
+
+            }
+
+
+
+            public ActionResult SaveImage()
+        {
+            var product = new Brand();
+
+            return View(product);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult ViewImage(int id)
+        {
+            var item = _context.Brands.Find(id);
+            byte[] buffer = item.BrandImage;
+            return File(buffer, "image/jpg", string.Format("{0}.jpg", id));
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SaveImage(HttpPostedFileBase uploadImages)
+        {
+          
+                if (uploadImages.ContentLength > 0)
+                {
+                    byte[] imageData = null;
+                    using (var binaryReader = new BinaryReader(uploadImages.InputStream))
+                    {
+                        imageData = binaryReader.ReadBytes(uploadImages.ContentLength);
+                    }
+                    var product = new Brand()
+                    {
+                        BrandImage = imageData
+
+                    };
+                    _context.Brands.Add(product);
+                }
+            
+            _context.SaveChanges();
+            return Content("Success!");
+        }
+
+    }
+}
---
+++ DemoUpload/Models/Brand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoUpload.Models
+{
+    public class Brand
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public byte[] BrandImage { get; set; }
+    }
+}
