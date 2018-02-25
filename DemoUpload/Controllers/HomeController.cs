9395522eb6ea6eca22085354dc3380e893bfc398
HeathcliffEarnshaw <36394558+HeathcliffEarnshaw@users.noreply.github.com>
2018-02-25 10:37:16 +0800

    AddImage and ViewImage

--- DemoUpload/Controllers/HomeController.cs
+++ DemoUpload/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DemoUpload.Models;
 
 namespace DemoUpload.Controllers
 {
@@ -25,6 +26,34 @@ namespace DemoUpload.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult AddImage()
+        {
+            Brand brandImage = new Brand();
+            return View(brandImage);
+        }
+
+        [HttpPost]
+        public ActionResult AddImage(Brand model, HttpPostedFileBase image1)
+        {
+            var db = new ApplicationDbContext();
+            if(image1 != null)
+            {
+                model.BrandImage = new byte[image1.ContentLength];
+                image1.InputStream.Read(model.BrandImage,0,image1.ContentLength);
+            }
+
+            db.Brands.Add(model);
+            db.SaveChanges();
+            return View(model);
+        }
+        public ActionResult ViewImage()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            var item = (from d in db.Brands
+                select d).ToList();
+            return View(item);
         }
     }
 }
